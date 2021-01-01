5ca47a4575e214c64ed32ade0a85135b7cf13503
Eric <eric.m.lombardo@gmail.com>
2021-01-01 13:34:27 -0500

    Bring in causes(themes) from API
    Brought in the API using HTTParty gem. Assigned a hash that includes 'id' and 'name' to themes variable to create new instances of Cause class.

--- lib/api.rb
+++ lib/api.rb
@@ -5,7 +5,11 @@ require 'json'
 class SimplyGive::API   # interact with the API
   def get_causes
     response = HTTParty.get("https://api.globalgiving.org/api/public/projectservice/themes?api_key=")
-    # binding.pry
-    response["themes"].values # this gives you back an array of hashes with "id" and "name"
+    themes = response["themes"].values.flatten # this gives you back an array of hashes with "id" and "name"
   end
 end
+
+
+# themes.each do |theme|    outputs the theme name
+  # puts theme.values[1]
+# end
