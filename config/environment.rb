2e6c801170267d169a66198a81ff5f4180fb91b7
Eric <eric.m.lombardo@gmail.com>
2021-01-08 06:12:16 -0500

    Clean methods and flow in CLI class
    Created methods to handle centering text and changing color. Created
    specific methods for each logo and divider. Simplified the methods I
    could right now.

--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,11 @@ require 'pry'
 # require models
 require_relative '../lib/api_keys.rb'
 require_relative "../lib/concerns/version"
-require_relative '../lib/cli.rb'
 require_relative '../lib/api.rb'
-require_relative '../lib/project.rb'
 require_relative '../lib/cause.rb'
 require_relative '../lib/charity.rb'
+require_relative '../lib/project.rb'
+require_relative '../lib/cli.rb'
 
 # require modules
 module SimplyGive
--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,132 +1,96 @@
 require_relative '../config/environment.rb'
 
 class SimplyGive::CLI   # interacts with the user
+
   attr_accessor :cause_num, :project_num, :project_set, :project
   
+  API = SimplyGive::API
+  CAUSE = SimplyGive::Cause
+  
   def call
-    welcome_screen
-    greet_user
-    start_from_causes
-  end
-
-  def start_from_causes
-    ask_for_cause
-    start_from_projects
-  end
-
-  def start_from_projects
-    ask_for_project
-    display_project_info
-    next_steps
-  end
-
-  def greet_user
-    puts
-    long_divider
-    puts "     __ _                 _           ___ _           ".colorize(:light_cyan)
-    puts "    / _(_)_ __ ___  _ __ | |_   _    / _ (_)_   _____ ".colorize(:light_cyan)
-    puts "    \\"" \\""| | '_ ` _ ""\\""| '_ ""\\""| | | | |  / /_""\\""/ ""\\"" ""\\"" / / _ ""\\".colorize(:light_cyan)
-    puts "    _""\\"" ""\\"" | | | | | | |_) | | |_| | / /_""\\""\\""| |""\\"" V /  __/".colorize(:light_cyan)
-    puts "    \\""__/_|_| |_| |_| .__/|_|""\\""__, | ""\\""____/|_| ""\\""_/ ""\\""___|".colorize(:light_cyan)
-    puts "                   |_|      |___/                     ".colorize(:light_cyan) 
-    puts                           
-    puts " " * 2 + "WHERE YOU CAN GIVE TO PROJECTS AND CHARITIES YOU LOVE.".colorize(:light_white) #54
-    puts 
-    short_divider
-    puts 
-    puts " " * 21 + "NAVIGATION TIPS:".colorize(:light_cyan)
-    puts "              * Enter ".colorize(:light_white) + "'m'".colorize(:light_red) + " to view menu".colorize(:light_white)
-    puts "         * Enter ".colorize(:light_white) + "'p'".colorize(:light_red) + " to view other projects".colorize(:light_white)
-    puts "                * Enter ".colorize(:light_white) + "'e'".colorize(:light_red) + " to exit".colorize(:light_white)
-    puts
-    puts "  * Press 'enter' to start your Simply Give experience".colorize(:light_white)
-    gets.strip
+      welcome
+      show_causes
+  end       
+  
+  def show_causes
+    get_cause
+    show_projects
+  end  
+  
+  def show_projects
+    get_project
+    show_project_info
   end
   
-  def ask_for_cause
-    get_causes_from_api if SimplyGive::Cause.all.empty?
-    puts
-    puts " " * 9 + "SELECT WHAT CAUSE YOU ARE INTERESTED IN".colorize(:light_cyan)
+  def get_cause
+    get_causes_from_api if CAUSE.all.empty?
+    space
+    text("SELECT WHAT CAUSE YOU ARE INTERESTED IN", :light_cyan)
     short_divider
-    puts
-    display_cause_names
+    CAUSE.all.each.with_index(1) {|cause, ind| text("#{ind}. #{cause.name}", :light_white)}
     get_cause_input_number
   end
   
-  def ask_for_project # displays instances of charities within cause, prompts answer, gets input until valid?
-    puts " "
+  def get_project # displays instances of charities within cause, prompts answer, gets input until valid?
+    space
     @project_set = get_projects_from_api 
-    puts "#{SimplyGive::Cause.all[@cause_num.to_i - 1].name.upcase}".colorize(:light_cyan)
-    puts "SELECT TO VIEW DETAILS".colorize(:light_cyan)
-    puts
+    text("SELECT TO SEE DETAILS OF PROJECTS THAT HELP WITH", :light_cyan)
+    text("#{@cause_num}", :light_cyan)
     short_divider
-    puts
     display_project_names
     get_project_input_number
-  end
-
-  def display_cause_names # show numbered list of cause names that are instances
-    SimplyGive::Cause.all.each.with_index(1) {|cause, ind| puts "              #{ind}.".colorize(:light_red) + " #{cause.name}".colorize(:light_white)}
   end
   
   def display_project_names
     case @project_set.count
     when 0
-      puts "No active projects listed for this cause.".colorize(:light_white)
+      text("No active projects listed for this cause.", :light_white)
     when 1
-      puts "#{@project_set.count}.".colorize(:light_red) +  "#{@project_set.name}".colorize(:light_white)
+      text("#{@project_set.count}. #{@project_set.name}", :light_white)
     else
       @project_set.each.with_index(1) do |project, ind|
-        puts "#{ind}.".colorize(:light_red) +  " #{project.name}".colorize(:light_white)
+        text("#{ind}. #{project.name}", :light_white)
       end
     end
-    puts "#{@project_set.count + 1}.".colorize(:light_red) +  " To See More Projects".colorize(:light_white) if SimplyGive::API.next_page != nil
+    text("#{@project_set.count + 1}. To See More Projects", :light_white) if API.next_page != nil
   end
 
-  def display_project_info
-    @project = @project_set[@project_num.to_i - 1]
-
-    puts "CHARITY:".colorize(:light_cyan) + " #{project.charity.name}".colorize(:light_white)
-    puts 
-    puts long_divider
-    puts
-    puts "PROJECT DESCRIPTION:".colorize(:light_cyan)
+  def show_project_info
+    text("CHARITY:", :light_cyan)
+    text("#{project.charity.name}", :light_white)
+    long_divider
+    text("PROJECT DESCRIPTION:", :light_cyan)
     puts "#{project.description}".colorize(:light_white)
-    puts
     long_divider
-    puts
-    puts "RAISED:".colorize(:light_cyan) + " $#{project.funds_raised} / $#{project.goal}".colorize(:light_white)
-    puts 
+    text("RAISED:", :light_cyan)
+    text("$#{project.funds_raised} / $#{project.goal}", :light_white)
     long_divider
-    puts
-    puts "ALL CAUSES THIS CHARITY SUPPORTS".colorize(:light_cyan)
-    puts
-    long_divider
-    puts
-    project.causes.each.with_index(1) {|cause, ind| (puts "#{ind}.".colorize(:light_red) + " #{cause.name}".colorize(:light_white))}
-    puts
+    text("ALL CAUSES THIS CHARITY SUPPORTS", :light_cyan)
+    space
+    @project.causes.each.with_index(1) {|cause, ind| text("#{ind}. #{cause.name}", :light_white)}
+    short_divider
+    next_steps
   end
   
   def get_cause_input_number
     @cause_num = gets.strip   # gets input
     if @cause_num == "p"
-      puts "Please select a cause to view projects".colorize(:light_white)
-      start_from_causes
+      text("Please select a cause to view projects", :light_white)
+      show_causes
     end
     check_input(@cause_num)
-    @cause_num.to_i.between?(1, total_causes) ? @cause_num : start_from_causes  # ask_for_cause until match, return input when it does
-
+    @cause_num.to_i.between?(1, total_causes) ? @cause_num = CAUSE.all[@cause_num.to_i - 1].name.upcase : show_causes  # get_cause until match, return input when it does
   end
   
   def get_project_input_number
-    @project_num = gets.strip
-    check_input(@project_num)
-    start_from_projects if @project_num == @project_set.count + 1
-    @project_num.to_i.between?(1, @project_set.count) ? @project_num : start_from_projects
+    @project = gets.strip
+    check_input(@project)
+    show_projects if @project == @project_set.count + 1
+    @project.to_i.between?(1, @project_set.count) ? @project = @project_set[@project.to_i - 1] : show_projects
   end
 
   def next_steps  # Ask for next step. Go to site or back or exit
-    puts
+    space
     puts "Enter ".colorize(:light_white) + "'g'".colorize(:light_red) + " to simply give".colorize(:light_white)
     input = gets.strip.downcase
     check_input(input)
@@ -136,48 +100,83 @@ class SimplyGive::CLI   # interacts with the user
   end
 
   def check_input(input) # checks for exit, causes, or projects
-    input = input.downcase
-    if input == "e"
+    case input.downcase
+    when "e"
       exit
-    elsif input == "m"
-      start_from_causes
-    elsif input == "p"
-   
-      start_from_projects
+    when "m"
+      show_causes
+    when "p"
+      show_projects
     end
   end
 
   def total_causes   # Gets length of all array in Cause class 
-    SimplyGive::Cause.all.count
+    CAUSE.all.count
   end
   
   def get_causes_from_api   
-    SimplyGive::API.new.get_causes
+    API.new.get_causes
   end
   
   def get_projects_from_api
-    SimplyGive::API.new.get_projects(cause: SimplyGive::Cause.all[@cause_num.to_i - 1], next_page: SimplyGive::API.next_page)
+    API.new.get_projects(cause: CAUSE.all[@cause_num.to_i - 1], next_page: API.next_page)
   end 
 
+  def text(text, color)   # not using keywords to try to clean up code
+    center_text = (57 - text.length) / 2
+    puts " " * center_text + text.colorize(color)
+  end
+
+
+
+  # GRAPHICS AND DISPLAY PATTERNS
+  # ==================================================================================
+  def welcome
+    border_logo   # display border to set screen size
+    gets
+    space
+    welcome_logo
+    space                                                                                                                                 
+    heart_logo                                                                                          
+    space    
+    simply_give_logo
+    sleep(4)
+  end
+  
+  def space(count: 1)
+    puts "\n" * count 
+  end
+
   def long_divider
-    puts " " * 2 + "><*><*><*><*><*><*><*><*><*><*><*><*><*><*><*><*><*><".colorize(:light_white)
+    puts
+    text("><*><*><*><*><*><*><*><*><*><*><*><*><*><*><*><*><*><", :light_red)
+    puts
   end
 
   def short_divider
-    puts " " * 13 + "><*><*><*><*><*><*><*><*><*><*><".colorize(:light_white)
+    puts
+    text("><*><*><*><*><*><*><*><*><*><*><", :light_red)
+    puts
   end
 
-  def welcome_screen
-    puts
-    puts " " * 6 + "Please make window 57 x 32 for optimal viewing".colorize(:light_white)
-    puts " " * 16 + "Press 'enter' to continue"
-    gets.strip
-    puts "   __    __     _                            ______     "
-    puts "  / / /""\\"" ""\\"" ""\\""___| | ___ ___  _ __ ___   ___  /__  __|__  "
-    puts "  ""\\"" ""\\""/  ""\\""/ / _ ""\\"" |/ __/ _ ""\\""| '_ ` _ ""\\"" / _ ""\\""   / / / _ ""\\"" "
-    puts "   ""\\""  /""\\""  /  __/ | (_| (_) | | | | | |  __/  / / | (_) |"
-    puts "    ""\\""/  ""\\""/ ""\\""___|_|""\\""___""\\""___/|_| |_| |_|""\\""___|  ""\\""/   ""\\""___/ "
-    puts                                                                                                                                 
+  def simply_give_logo
+    puts "     __ _                 _           ___ _           ".colorize(:light_white)
+    puts "    / _(_)_ __ ___  _ __ | |_   _    / _ (_)_   _____ ".colorize(:light_white)
+    puts "    \\"" \\""| | '_ ` _ ""\\""| '_ ""\\""| | | | |  / /_""\\""/ ""\\"" ""\\"" / / _ ""\\".colorize(:light_white)
+    puts "    _""\\"" ""\\"" | | | | | | |_) | | |_| | / /_""\\""\\""| |""\\"" V /  __/".colorize(:light_white)
+    puts "    \\""__/_|_| |_| |_| .__/|_|""\\""__, | ""\\""____/|_| ""\\""_/ ""\\""___|".colorize(:light_white)
+    puts "                   |_|      |___/                     ".colorize(:light_white) 
+  end
+
+  def welcome_logo
+    puts "   __    __     _                            ______     ".colorize(:light_white)
+    puts "  / / /""\\"" ""\\"" ""\\""___| | ___ ___  _ __ ___   ___  /__  __|__  ".colorize(:light_white)
+    puts "  ""\\"" ""\\""/  ""\\""/ / _ ""\\"" |/ __/ _ ""\\""| '_ ` _ ""\\"" / _ ""\\""   / / / _ ""\\"" ".colorize(:light_white)
+    puts "   ""\\""  /""\\""  /  __/ | (_| (_) | | | | | |  __/  / / | (_) |".colorize(:light_white)
+    puts "    ""\\""/  ""\\""/ ""\\""___|_|""\\""___""\\""___/|_| |_| |_|""\\""___|  ""\\""/   ""\\""___/ ".colorize(:light_white)
+  end
+
+  def heart_logo
     puts "        ......................................".colorize(:light_cyan)
     puts "        .....".colorize(:light_cyan) + ":::::::::::".colorize(:red) + "......".colorize(:light_cyan) + ":::::::::::".colorize(:red) + ".....".colorize(:light_cyan)
     puts "        ...".colorize(:light_cyan) + "::".colorize(:red) + "kXWMMMMMMNl".colorize(:red) + "::".colorize(:red) + "..".colorize(:light_cyan) + "::".colorize(:red) + "lONMMMMMWXx".colorize(:red) + "::".colorize(:red) + "...".colorize(:light_cyan)
@@ -195,14 +194,41 @@ class SimplyGive::CLI   # interacts with the user
     puts "        .............".colorize(:light_cyan) + "::".colorize(:red) + "oONNOo".colorize(:red) + "::".colorize(:red) + "...............".colorize(:light_cyan)
     puts "        ...............".colorize(:light_cyan) + "::".colorize(:red) + "dd".colorize(:red) + "::".colorize(:red) + ".................".colorize(:light_cyan)
     puts "        .................".colorize(:light_cyan) + "::".colorize(:red) + "...................".colorize(:light_cyan)
-    puts "        ......................................".colorize(:light_cyan)                                                                                          
-    puts    
-    puts "   __ _                 _           ___ _           ".colorize(:light_white)
-    puts "  / _(_)_ __ ___  _ __ | |_   _    / _ (_)_   _____ ".colorize(:light_white)
-    puts "  \\"" \\""| | '_ ` _ ""\\""| '_ ""\\""| | | | |  / /_""\\""/ ""\\"" ""\\"" / / _ ""\\".colorize(:light_white)
-    puts "  _""\\"" ""\\"" | | | | | | |_) | | |_| | / /_""\\""\\""| |""\\"" V /  __/".colorize(:light_white)
-    puts "  \\""__/_|_| |_| |_| .__/|_|""\\""__, | ""\\""____/|_| ""\\""_/ ""\\""___|".colorize(:light_white)
-    puts "                 |_|      |___/                     ".colorize(:light_white)
-    sleep(5)
+    puts "        ......................................".colorize(:light_cyan) 
+  end
+
+  def border_logo
+    puts " .--..--..--..--..--..--..--..--..--..--..-..--..--..--.".colorize(:light_cyan)
+    puts "/ .. ""\\"".. ""\\"".. ""\\"".. ""\\"".. ""\\"".. ""\\"".. ""\\"".. ""\\"".. ""\\"".. ""\\"". ""\\"".. ""\\"".. ""\\"".. ""\\""".colorize(:light_cyan)
+    puts "\\"" ""\\""/""\\"" ""\\""/""\\"" ""\\""/""\\"" ""\\""/""\\"" ""\\""/""\\"" ""\\""/""\\"" ""\\""/""\\"" ""\\""/""\\"" ""\\""/""\\"" ""\\""/""\\"" ""\\""""\\"" ""\\""/""\\"" ""\\""/""\\"" ""\\""/ /".colorize(:light_cyan)
+    puts " ""\\""/ /""\\""/ /""\\""/ /""\\""/ /""\\""/ /""\\""/ /""\\""/ /""\\""/ /""\\""/ /""\\""/ /""\\""//""\\""/ /""\\""/ /""\\""/ /".colorize(:light_cyan)
+    puts " / /""\\""/ /`' /`' /`' /`' /`' /`' /`' /`' /`' `' /`' /""\\""/ /""\\""".colorize(:light_cyan)
+    puts "/ /""\\"" ""\\""/`--'`--'`--'`--'`--'`--'`--'`--'`--'--'`--'""\\"" ""\\""/""\\"" ""\\""".colorize(:light_cyan)
+    puts """\\"" ""\\""/""\\"" ""\\""                                           /""\\"" ""\\""/ /".colorize(:light_cyan)
+    puts " ""\\""/ /""\\"" ""\\""                                         / /""\\""/ /".colorize(:light_cyan)
+    puts " / /""\\""/ /                                         ""\\"" ""\\""/ /""\\""".colorize(:light_cyan)
+    puts "/ /""\\"" ""\\""/                                           ""\\"" ""\\""/""\\"" ""\\""".colorize(:light_cyan)
+    puts """\\"" ""\\""/""\\"" ""\\""                                           /""\\"" ""\\""/ /".colorize(:light_cyan)
+    puts " ""\\""/ /""\\"" ""\\".colorize(:light_cyan) + "        PLEASE MAKE SURE YOU CAN".colorize(:light_white) + "         / /""\\""/ /".colorize(:light_cyan)
+    puts " / /""\\""/ /".colorize(:light_cyan) + "         SEE THE ENTIRE BOARDER".colorize(:light_white) + "          ""\\"" ""\\""/ /""\\""".colorize(:light_cyan)
+    puts "/ /""\\"" ""\\""/".colorize(:light_cyan) +  "             FOR BEST VIEWING".colorize(:light_white) + "              ""\\"" ""\\""/""\\"" ""\\""".colorize(:light_cyan)
+    puts "\\"" " "\\""/""\\"" ""\\".colorize(:light_cyan) + "           PRESS ENTER TO START".colorize(:light_white) + "            /""\\"" ""\\""/ /".colorize(:light_cyan)
+    puts " ""\\""/ /""\\"" ""\\""                                         / /""\\""/ /".colorize(:light_cyan)
+    puts " / /""\\""/ /                                         ""\\"" ""\\""/ /""\\""".colorize(:light_cyan)
+    puts "/ /""\\"" ""\\""/                                           ""\\"" ""\\""/""\\"" ""\\""".colorize(:light_cyan)
+    puts """\\"" ""\\""/""\\"" ""\\""                                           /""\\"" ""\\""/ /".colorize(:light_cyan)
+    puts " ""\\""/ /""\\"" ""\\""                                         / /""\\""/ /".colorize(:light_cyan)
+    puts " / /""\\""/ /                                         ""\\"" ""\\""/ /""\\""".colorize(:light_cyan)
+    puts "/ /""\\"" ""\\""/                                           ""\\"" ""\\""/""\\"" ""\\""".colorize(:light_cyan)
+    puts """\\"" ""\\""/""\\"" ""\\""                                           /""\\"" ""\\""/ /".colorize(:light_cyan)
+    puts " ""\\""/ /""\\"" ""\\""                                         / /""\\""/ /".colorize(:light_cyan)
+    puts " / /""\\""/ /                                         ""\\"" ""\\""/ /""\\""".colorize(:light_cyan)
+    puts "/ /""\\"" ""\\""/                                           ""\\"" ""\\""/""\\"" ""\\""".colorize(:light_cyan)
+    puts """\\"" ""\\""/""\\"" ""\\"".--..--..--..--..--..--..--..--..--.--..--./""\\"" ""\\""/ /".colorize(:light_cyan)
+    puts " ""\\""/ /""\\""/ ../ ../ ../ ../ ../ ../ ../ ../ ../../ ../ /""\\""/ /".colorize(:light_cyan)
+    puts " / /""\\""/ /""\\""/ /""\\""/ /""\\""/ /""\\""/ /""\\""/ /""\\""/ /""\\""/ /""\\""/ /""\\""/ ""\\""/ /""\\""/ /""\\""/ /""\\""".colorize(:light_cyan)
+    puts "/ /""\\"" ""\\""/""\\"" ""\\""/""\\"" ""\\""/""\\"" ""\\""/""\\"" ""\\""/""\\"" ""\\""/""\\"" ""\\""/""\\"" ""\\""/""\\"" ""\\""/""\\"" ""\\""/ ""\\""/""\\"" ""\\""/""\\"" ""\\""/""\\"" ""\\""".colorize(:light_cyan)
+    puts """\\"" `'""\\"" `'""\\"" `'""\\"" `'""\\"" `'""\\"" `'""\\"" `'""\\"" `'""\\"" `'""\\"" `'""\\"" `""\\"" `'""\\"" `'""\\"" `' /".colorize(:light_cyan)
+    puts " `--'`--'`--'`--'`--'`--'`--'`--'`--'`--'`-'`--'`--'`--'".colorize(:light_cyan)
   end
 end
